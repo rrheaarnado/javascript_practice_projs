f71abb77f6bd4b6111afec036723c15277991283
Rhea Mae Arnado <102829663+rrheaarnado@users.noreply.github.com>
2025-02-27 17:11:33 +0800

    commit

---
+++ 4_navbar/app.js
@@ -0,0 +1,27 @@
+// classList - shows/gets all classes
+// contains - checks classList for specific class
+// add - add class
+// remove - remove class
+// toggle - toggles class
+
+const navToggle = document.querySelector(".nav-toggle-btn");
+const links = document.querySelector(".links");
+
+
+navToggle.addEventListener("click", function() {
+
+    // console.log(links.classList);
+    // console.log(links.classList.contains("random"));
+    // console.log(links.classList.contains("english"));
+    // console.log(links.classList.contains("links"));
+
+    // ADD REMOVE METHOD
+    // if(links.classList.contains("show-links")){
+    //     links.classList.remove("show-links");
+    // }else{
+    //     links.classList.add("show-links");
+    // }
+    
+    //TOGGLE METHOD
+    links.classList.toggle("show-links");
+});
